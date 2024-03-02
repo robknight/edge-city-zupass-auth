4455b8c958363a82f63f71842fabea2325d40578
Rob Knight <mail@robknight.org.uk>
2024-03-02 15:10:26 -0700

    Initial commit

---
+++ src/EdgeCityTickets.ts
@@ -0,0 +1,141 @@
+export const ticketPatterns = {
+  "Edge City Denver - GA": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "e1dc58c3-2089-4c17-b282-bc37ddbb30b0"
+  },
+  "Edge City Denver - Early Bird": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "881ceef7-e38c-4611-9de2-1506d13b8f48"
+  },
+  "Edge City Denver - Organizer": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "2127ea78-4144-4850-8847-b1bec9e53bd1"
+  },
+  "Edge City Denver - Monday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "d57f9514-e740-4334-b2db-a0389840e25c"
+  },
+  "Edge City Denver - Tuesday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "1e53c507-077e-4cee-a386-0a42e63fe2f8"
+  },
+  "Edge City Denver - Wednesday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "b8696011-93ca-4710-8bce-6136a35128d3"
+  },
+  "Edge City Denver - Thursday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "eb58f450-8025-4d97-9811-1008afd22319"
+  },
+  "Edge City Denver - Friday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "fbc732ea-abf4-4428-a422-7855725ebc90"
+  },
+  "Edge City Denver - Saturday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "be109df0-5a65-4f58-a36c-5dcfc5170a8c"
+  },
+  "Edge City Denver - Sunday Day Pass": {
+    eventId: "7eb74440-1891-4cd5-a351-b24a5b03e669",
+    productId: "0eede921-5bdc-45e0-bfab-5cb71db201c0"
+  },
+}
+
+export const badgePatterns = {
+  Star: {
+    eventId: "c87e2dd9-294e-59c1-8235-b800e9ec7b61",
+    productId: "15d23b58-a2b3-5f09-856f-960355f0445c"
+  },
+  "Check In": {
+    eventId: "60feda93-675d-5707-9ad0-909ba071488d",
+    productId: "00d024b4-6aac-5699-8415-6cdce48d46f3"
+  },
+  "Cold Plunge.Monday": {
+    eventId: "5dc4f2b9-02c3-5cea-85ee-71bd7a539054",
+    productId: "87409dc6-d8e8-5206-afd0-ac55ac460462"
+  },
+  "Cold Plunge.Tuesday": {
+    eventId: "bc76e6ee-adfb-52cb-ba20-5c3cdc8954bb",
+    productId: "03538882-5fa8-5d83-b11e-c64575090f8d"
+  },
+  "Cold Plunge.Wednesday": {
+    eventId: "a76a71cf-4405-52ab-bfae-f9689f935332",
+    productId: "d49731f3-10ee-5051-9178-3e4af937077c"
+  },
+  "Cold Plunge.Thursday": {
+    eventId: "3340b7c9-690a-5d16-a4e3-d210595bd3ff",
+    productId: "1d6088e7-dbfa-56e5-b026-7b9141728e53"
+  },
+  "Cold Plunge.Friday": {
+    eventId: "394c2293-5030-54d2-86f5-33694bcc3ccf",
+    productId: "43151a63-8389-5eef-9035-34bb3f485ef3"
+  },
+  "Cold Plunge.Saturday": {
+    eventId: "5a1a4fcb-8536-515e-afe8-c7620b37df5f",
+    productId: "5f588104-0f62-5051-9eb5-17da8c7ba01f"
+  },
+  "Cold Plunge.Sunday": {
+    eventId: "53df61fc-bf9d-59b0-ba6c-4b5ce60f4466",
+    productId: "1abc97c0-1fd5-5667-8492-d86a60044392"
+  },
+  "Sauna.Tuesday": {
+    eventId: "6e5bfd47-078f-5015-8034-09b4951b2d18",
+    productId: "5e6de144-0dfb-56ee-a46f-354a46d7a917"
+  },
+  "Sauna.Wednesday": {
+    eventId: "47e78dc3-2709-5c93-9eba-bea4a5216cf3",
+    productId: "97231f81-25fc-5a7e-8290-d8798fcd4119"
+  },
+  "Sauna.Thursday": {
+    eventId: "62bcf80f-d8bc-5766-b269-49754e493b10",
+    productId: "740edcd5-ff25-50a3-ab9d-39ba3259184f"
+  },
+  "Sauna.Friday": {
+    eventId: "bfa4325b-0999-5e51-b41f-f166b08e253f",
+    productId: "18515572-1d31-5cbd-aa81-2171861da302"
+  },
+  "Sauna.Saturday": {
+    eventId: "1a2fa28d-699c-5f56-b602-a729aa8e7b11",
+    productId: "b7ee0fca-c2c0-5e65-a465-8945f03858d6"
+  },
+  "Sauna.Sunday": {
+    eventId: "2dd2c371-46cb-5a7e-97ee-5f4d13685994",
+    productId: "1f19268e-563f-567c-a9a6-0c560a29728a"
+  },
+  "Met Zupass.Josh": {
+    eventId: "6bfc7ffe-ab04-5684-bc0b-e7bb45743845",
+    productId: "232c0468-9414-5b05-9373-2f2c53acef82"
+  },
+  "Met Zupass.Richard": {
+    eventId: "e76dc05e-6e9d-5d7d-ac0b-ebdb7e1a5c75",
+    productId: "9a8ff770-0e12-5720-b5d8-ecb3eca80cec"
+  },
+  "Met Zupass.Ivan": {
+    eventId: "7ceebbaf-9287-5caf-91fc-03c1a3f59cd7",
+    productId: "72fbbeb3-b1d7-514b-a560-0f2284bef5a5"
+  },
+  "Met Zupass.Rob": {
+    eventId: "834bd506-600c-5b8e-8248-5dc65b419a1d",
+    productId: "180a269b-0f45-5853-b849-2ce8c9116c75"
+  },
+  "Met Gary.Monday": {
+    eventId: "4530479d-4a28-5f86-b76d-78d31c555df2",
+    productId: "15482abf-e743-5fc3-a8e8-1245773683fd"
+  },
+  "Met Timour": {
+    eventId: "a16d238c-9c97-57b8-94a4-b746ec7b2e7b",
+    productId: "81292214-ad53-5c06-b563-5fbe0535c841"
+  },
+  "Met Afra": {
+    eventId: "9fbbb464-da41-57af-b192-893aee19c3bf",
+    productId: "a39789fa-97b3-5a5e-bc74-3c56531ecdab"
+  },
+  "Met Arjun": {
+    eventId: "5d8c2082-4806-5d45-b87d-9c77b3d922d8",
+    productId: "75b68e9c-f6b0-5e65-a9c9-55a121e1d00a"
+  },
+  "Decentralized Social Hacker": {
+    eventId: "777e692b-e6ae-5277-ac45-9c4216240369",
+    productId: "df902261-fc81-5b76-a538-955fd81d96be"
+  }
+};
